98b0d1b93c0b00bfb13f89ec3caf58c5ed4d87cd
= <=>
2025-09-22 18:46:37 -0300

    Feat: loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <span className="loader">..carregando Pedro Brito</span>
+    <div className="flex items-center justify-center min-h-screen bg-neutral-900">
+      <div className="w-16 h-16 border-4 border-gray-300 border-t-purple-900 rounded-full animate-spin"></div>
     </div>
   );
 }

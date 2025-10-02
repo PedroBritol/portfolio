3a62ccc1bf2b81529324b82a1d459057cbf3f01a
= <=>
2025-10-02 20:47:00 -0300

    fix: espaçamento navbar

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 left-0 z-50 flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-neutral-900 backdrop-blur-sm text-sm py-3 dark:bg-neutral-900">
-      <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+      <nav className="container w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
         <div className="flex items-center justify-between">
           <Link href="/" className="flex-none text-xl font-semibold dark:text-white focus:outline-hidden focus:opacity-80" aria-label="Brand" onClick={handleLinkClick}>
             <span className="inline-flex items-center gap-x-2 text-xl font-semibold text-white">

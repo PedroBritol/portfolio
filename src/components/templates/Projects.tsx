1443d54bffadc9bda423635f989e21573c5def16
= <=>
2025-10-15 11:28:39 -0300

    :fire: cleanup: limpando codigo comentados/n utlizados

--- src/components/templates/Projects.tsx
+++ src/components/templates/Projects.tsx
@@ -1,12 +1,5 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Autoplay, A11y } from 'swiper/modules';
 import ProjectCard from './../cards/ProjectCard';
 import {projectsData} from '@/data/projectsData';
-
-
 
   export default function Projects() {
     return (
@@ -25,40 +18,6 @@ import {projectsData} from '@/data/projectsData';
                 />
               ))}
           </div>
-          {/* <div className="mt-8 py-8">
-            <Swiper
-              modules={[Navigation, Pagination, Autoplay, A11y]}
-              navigation={false}
-              loop={true}
-              autoplay={{
-                delay: 3000,
-                disableOnInteraction: false,
-              }}
-              pagination={{ clickable: true }}
-              spaceBetween={0}
-              slidesPerView={1}
-              centeredSlides={true}
-              grabCursor={true}
-              breakpoints={{
-                480: { slidesPerView: 2, spaceBetween: 0 },
-                640: { slidesPerView: 2, spaceBetween: 20 },
-                1024: { slidesPerView: 2, spaceBetween: 30 },
-              }}
-              className="custom-swiper !pb-12 !py-3"
-            >
-              {projects.map((card) => (
-                <SwiperSlide key={card.id}>
-                  <ProjectCard
-                    id={card.id}
-                    imageUrl={card.imageUrl}
-                    title={card.title}
-                    description={card.description}
-                    stack={card.stack}
-                  />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div> */}
         </div>
       </section>
     );
